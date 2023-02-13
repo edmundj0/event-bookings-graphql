adf7ab056016a763c9b58c51f37e7ec3254da974
Edmund Ju <edmundju@berkeley.edu>
2023-02-13 00:33:03 -0800

    add schema and resolver

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql'); //middleware to parse and handle graphql requests
+
+
+const { buildSchema } = require('graphql');
 
 const app = express()
 
 app.use(bodyParser.json()); //to parse incoming json bodies
+
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+
+    `),
+    //resolvers
+    rootValue: {
+        events: () => {
+            return ["test event", "test event 2"]
+        },
+        createEvent: (args) => {
+            const eventName = args.name
+            return eventName
+        }
+    },
+    graphiql: true //for api testing
+}))
+
+
+
 
 app.listen(3000);

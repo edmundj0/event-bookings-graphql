f4dc1a28fc8be7cf6b972e9c8e008c31561a7573
Edmund Ju <edmundju@berkeley.edu>
2023-04-01 13:49:59 -0700

    add pages

---
+++ frontend/src/pages/Auth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AuthPage extends Component {
+    render() {
+        return <h1>Auth Page</h1>
+    }
+}
+
+// function AuthPage() {
+//     return <h1>test</h1>
+// }
+
+export default AuthPage
---
+++ frontend/src/pages/Bookings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BookingsPage extends Component {
+    render() {
+        return <h1>Bookings Page</h1>
+    }
+}
+
+// function AuthPage() {
+//     return <h1>test</h1>
+// }
+
+export default BookingsPage

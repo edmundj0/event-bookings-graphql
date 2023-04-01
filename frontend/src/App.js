1f9274e296b7273eb2d369100281098214c46f22
Edmund Ju <edmundju@berkeley.edu>
2023-04-01 14:30:14 -0700

    create navbar and style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
+import MainNavigation from './components/Navigation/MainNavigation';
 import AuthPage from './pages/Auth'
 import EventsPage from './pages/Events'
 import BookingsPage from './pages/Bookings'
@@ -10,13 +11,16 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          {/* <Route path="/" component={null} exact /> */}
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventsPage} />
-          <Route path="/bookings" component={BookingsPage} />
+        <MainNavigation />
+        <main className='main-content'>
+          <Switch>
+            <Redirect from="/" to="/auth" exact />
+            {/* <Route path="/" component={null} exact /> */}
+            <Route path="/auth" component={AuthPage} />
+            <Route path="/events" component={EventsPage} />
+            <Route path="/bookings" component={BookingsPage} />
           </Switch>
+        </main>
       </BrowserRouter>
     </div>
   );

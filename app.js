899e29df75b711d6d04b53b5a7fe6ac2ce6834b5
Edmund Ju <edmundju@berkeley.edu>
2023-04-01 20:37:53 -0700

    hit backend for create user

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ const app = express()
 app.use(bodyParser.json()); //to parse incoming json bodies
 
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS'){
+        return res.sendStatus(200)
+    }
+    next()
+})
+
+
 app.use(isAuth) //will never throw an error, only checks if is authorized
 
 
--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -2,17 +2,55 @@ import React, { Component } from 'react';
 import './Auth.css';
 
 class AuthPage extends Component {
+    constructor(props) {
+        super(props);
+        this.emailEle = React.createRef();
+        this.passwordEle = React.createRef()
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        const email = this.emailEle.current.value
+        const password = this.passwordEle.current.value
+
+        if (email.trim().length === 0 || password.trim().length === 0) {
+            return
+        }
+
+        const requestBody = {
+            query: `
+                mutation {
+                    createUser(userInput: {email: "${email}", password: "${password}"}) {
+                        _id
+                        email
+                    }
+                }
+            `
+        }
+
+        fetch('http://localhost:5000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+    }
+
+
+
     render() {
         return (<>
             <h1>Auth Page</h1>
-            <form className='auth-form'>
+            <form className='auth-form' onSubmit={this.submitHandler}>
                 <div className='form-control'>
                     <label htmlFor='email'>E-Mail</label>
-                    <input type='email' id="email" />
+                    <input type='email' id="email" ref={this.emailEle} />
                 </div>
                 <div className='form-control'>
                     <label htmlFor='password'>Password</label>
-                    <input type='password' id="password" />
+                    <input type='password' id="password" ref={this.passwordEle} />
                 </div>
                 <div className='form-actions'>
                     <button type='submit'>Submit</button>
